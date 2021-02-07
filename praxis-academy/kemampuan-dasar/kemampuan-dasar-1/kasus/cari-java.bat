8bea077354013fe21b01825aa536a6967a371b24
tasyaulfha <17523184@students.uii.ac.id>
2021-02-07 21:53:37 +0700

    Kemampuan-dasar

---
+++ praxis-academy/kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.bat
@@ -0,0 +1,20 @@
+﻿function carijava{
+
+
+    param
+    (
+        [Parameter(Mandatory=$true)][string]$namadirektori
+    )
+    
+    $carijava=Get-Childitem -Path $Path -Include *.java -Recurse
+    
+    if($carijava)
+    {
+        Write-Host "Ada file Java pada direktori {$namadirektori}"
+    }
+    else
+    {
+        Write-Host "Tidak ada file Java yang ditemukan"
+    }
+
+}
---
+++ praxis-academy/kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,48 @@
+﻿function carijava{
+
+    param
+    (
+        [Parameter(Mandatory=$true)][string]$direktori
+        [Parameter(Mandatory=$true)][string]$gantinama
+    )
+    
+    Rename-Item $direktori $gantinama
+    
+   Write-Host "File di $direktori, sudah berganti nama menjadi $gantinama"
+    
+}
+function pilih{
+
+    param(
+        [Parameter(Mandatory=$true)][string]$search,
+        [Parameter(Mandatory=$true)][string]$ya_tidak
+    )
+
+    if($ya_tidak -eq "y")
+    {
+    gantinama $carijava
+    }
+    else
+    {
+    Write-Host "Lanjut ke langkah berikutnya"
+    }
+    function carijava{
+
+     param
+     (
+         [Parameter(Mandatory=$true)][string]$direktori
+     )
+
+     $carijava=Get-Childitem -Path $Path -Include *.java -Recurse
+
+     if($carijava)
+     {
+         Write-Host "Ada file Java pada direktori {$direktori}"
+     }
+     else
+     {
+         Write-Host "Tidak ada file Java yang ditemukan"
+     }
+
+ }
+ carijava
