1cf7eed9a3cb771778b6b2b1ee5665ec5af765b7
tasyaulfha <tasya@tasya>
2021-02-09 16:11:48 +0700

    perbaikan

--- Navice/01-02/latihan/cobaGradle/app/src/main/java/cobagradle/App.java
+++ Navice/01-02/latihan/cobaGradle/app/src/main/java/cobagradle/App.java
@@ -7,30 +7,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-    int i, j, jumlahBaris, jumlahKolom;
+    int i, j, m, n;
     int matriks1[][] = new int[10][10];
     int matriks2[][] = new int[10][10];
     int hasil[][] = new int[10][10];
     Scanner scan = new Scanner(System.in);
     System.out.print("Masukkan jumlah baris matriks: ");
-    jumlahBaris = scan.nextInt();
+    m = scan.nextInt();
     System.out.print("Masukkan jumlah kolom matriks: ");
-    jumlahKolom = scan.nextInt();
+    n = scan.nextInt();
     System.out.println("Masukkan elemen matriks pertama: ");
-    for (i = 0; i < jumlahBaris; i++) {
-      for (j = 0; j < jumlahKolom; j++) {
+    for (i = 0; i < m; i++) {
+      for (j = 0; j < n; j++) {
         matriks1[i][j] = scan.nextInt();
       }
     }
     System.out.println("Masukkan elemen matriks kedua: ");
-    for (i = 0; i < jumlahBaris; i++) {
-      for (j = 0; j < jumlahKolom; j++) {
+    for (i = 0; i < m; i++) {
+      for (j = 0; j < n; j++) {
         matriks2[i][j] = scan.nextInt();
       }
     }
     System.out.println("Hasil penjumlahan matriks: ");
-    for (i = 0; i < jumlahBaris; i++) {
-      for (j = 0; j < jumlahKolom; j++) {
+    for (i = 0; i < m; i++) {
+      for (j = 0; j < n; j++) {
         hasil[i][j] = matriks1[i][j] + matriks2[i][j];
         System.out.print(hasil[i][j] + "\t");
       }
