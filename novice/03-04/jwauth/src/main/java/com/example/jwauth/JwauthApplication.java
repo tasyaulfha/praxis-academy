1f42b52b77ae89e4c02953635e24cbdcd10d4553
tasyaulfha <17523184@students.uii.ac.id>
2021-02-25 20:17:12 +0700

    belajar Jwt

---
+++ novice/03-04/jwauth/src/main/java/com/example/jwauth/JwauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwauthApplication.class, args);
+	}
+
+}
