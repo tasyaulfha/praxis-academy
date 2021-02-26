565331e6d1ea8fc056cc10e6cdd74e7c47198f54
tasyaulfha <17523184@students.uii.ac.id>
2021-02-26 14:18:56 +0700

    Jwt auth

--- novice/03-05/lanjutan buku/buku/src/main/java/com/example/jwauth/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jwauth.repository;
-
-import com.example.jwauth.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
